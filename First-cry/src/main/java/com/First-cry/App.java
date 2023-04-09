506d4fcada0aa0af4604301ef0f6d4baec07a677
pallu-git <hardik1234@gmail.com>
2023-04-09 01:08:57 -0400

    maven projects

---
+++ First-cry/src/main/java/com/First-cry/App.java
@@ -0,0 +1,13 @@
+package com.First-cry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
